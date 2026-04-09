6f7a5a7003ef898fc913d4ba233b2d1db9ca0088
Josephsaku <Josephsakureal@gmail.com>
2026-04-09 21:50:27 +0800

    Update: Add GoatCounter analytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: 'goatcounter',
+      websiteId: 'johannes-garden',
     },
     locale: "en-US",
     baseUrl: "josephsaku.github.io",
